f426fa3c05888e72fef03aab4cf722e021c50716
Vyacheslav Koscheev <v.koscheev@2gis.ru>
2018-02-27 16:49:20 +0700

    cmake build version

--- test.sh
+++ test.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 [ "$NDK_ROOT" != "" ] || echo "Need to set NDK_ROOT!"
+[ "$ANDROID_CMAKE" != "" ] || echo "Need to set ANDROID_CMAKE!"
 
-ARCH=x86 # Comment to build arm
+rm -rf build && mkdir build && cd build
 
-if [ "$ARCH" == "x86" ]; then
-	SYSROOT=$NDK_ROOT/platforms/android-19/arch-x86
-	GCC_TOOLCHAIN=$NDK_ROOT/toolchains/x86-4.9/prebuilt/linux-x86_64
-	TARGET=i686-none-linux-android
-else
-	SYSROOT=$NDK_ROOT/platforms/android-19/arch-arm
-	GCC_TOOLCHAIN=$NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64
-	TARGET=armv7-none-linux-androideabi
-fi
+$ANDROID_CMAKE \
+    .. \
+    -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake \
+    -DANDROID_NDK=$NDK_ROOT \
+    -DANDROID_NATIVE_API_LEVEL=19 \
+    -DANDROID_ABI=x86 \
+    -DANDROID_TOOLCHAIN=clang \
+    -DCMAKE_BUILD_TYPE=Release \
+&& make
 
-CXX=$NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++
-
-CXX_OPTIONS="--sysroot=$SYSROOT -target $TARGET -O0 -std=c++14"
-LD_OPTIONS="--sysroot=$SYSROOT -target $TARGET -gcc-toolchain $GCC_TOOLCHAIN -pie"
-
-$CXX $CXX_OPTIONS -c main.cpp -o main.o && \
-$CXX $LD_OPTIONS -o test main.o && \
-adb push test /data/local/tmp/test && \
+adb push test_segfault /data/local/tmp/test && \
 adb shell chmod +x /data/local/tmp/test && \
 adb shell "cd /data/local/tmp && ./test && echo Success! || echo Failed! "
